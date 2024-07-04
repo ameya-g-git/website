1e14c8b3e294ac971fcbb48fd40d32e81f611f6d
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-07-03 22:04:54 -0400

    fixed implementation of getrepodetails

--- src/hooks/getRepoDetails.ts
+++ src/hooks/getRepoDetails.ts
@@ -2,36 +2,47 @@ import { Octokit } from 'octokit';
 const octokit = new Octokit({ })
 
 export async function getRepoDetails(repoName : string) {
+    let img : string | null;
+    let description : string | null;
+    let languages : string[] | null;
+
     try {
-        const img : any = await octokit.request('GET /repos/{owner}/{repo}/contents/assets/{path}', {
+        img = await octokit.request('GET /repos/{owner}/{repo}/contents/assets/{path}', {
             owner: 'ameya-g-git',
             repo: repoName,
             path: 'image.png'
-        }).then(res => { return res.data.download_url
-        }).catch(err => { console.log(err); return null})
-        
-        const languages : any = await octokit.request('GET /repos/{owner}/{repo}/languages', {
-            owner: 'ameya-g-git',
-            repo: repoName
-        }).then(res => {return Object.keys(res.data).map( (item : string) => item.toLowerCase()).sort()
-        }).catch(err => {console.log(err); return null})
-
-        const description : any = await octokit.request('GET /repos/{owner}/{repo}', {
-            owner: 'ameya-g-git',
-            repo: repoName
-        }).then(res => {return res.data.description
-        }).catch(err => {console.log(err); return null})
-
-        return (
-            {
-                name: repoName,
-                img: img ? img : null,
-                description: description ? description : null,
-                languages: languages ? languages : null,
-            }
-        );
+        }).then(res => { return res.data.download_url})
     } catch (error) {
-        console.error(`Error fetching details for repository ${repoName}:`, error);
-        return null;
+        console.error(`Error fetching image for repository ${repoName}:`, error);
+        img = ""
     }
+
+    try {
+        description = await octokit.request('GET /repos/{owner}/{repo}', {
+            owner: 'ameya-g-git',
+            repo: repoName
+        }).then(res => {return res.data.description})
+    } catch (error) {
+        console.error(`Error fetching desc. for repository ${repoName}:`, error);
+        description = ""
+    }
+
+    try {
+        languages = await octokit.request('GET /repos/{owner}/{repo}/languages', {
+            owner: 'ameya-g-git',
+            repo: repoName
+        }).then(res => {return Object.keys(res.data).map( (item : string) => item.toLowerCase()).sort()})
+    } catch (error) {
+        console.error(`Error fetching languages for repository ${repoName}:`, error);
+        languages = []
+    }
+
+    return (
+        {
+            name: repoName,
+            img: img,
+            description: description ? description : null,
+            languages: languages,
+        }
+    );
 }
