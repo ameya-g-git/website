50d187583edb95d960d80861521bd89581365dfd
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-01-23 20:16:44 -0500

    revised resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,11 +32,11 @@ export default function Navbar() {
 		>
 			{screenWidth > 768 && (
 				<ScrollingImage ltr={true} width="starbar">
-					<img src={starbar} alt="" className="object-cover w-64" />
+					<img src={starbar} alt="" className="w-64 object-cover" />
 				</ScrollingImage>
 			)}
 			<nav className="w-full *:text-sm md:*:text-base">
-				<ul className="relative inline-flex items-center justify-around w-full h-full px-8 text-lg transition-all font-body text-yellow">
+				<ul className="font-body relative inline-flex h-full w-full items-center justify-around px-8 text-lg text-yellow transition-all">
 					{navLinkElements}
 					<li>
 						<a
@@ -57,7 +57,7 @@ export default function Navbar() {
 			</nav>
 			{screenWidth > 768 && (
 				<ScrollingImage ltr={false} width="starbar">
-					<img src={starbar} alt="" className="object-cover w-64" />
+					<img src={starbar} alt="" className="w-64 object-cover" />
 				</ScrollingImage>
 			)}
 		</div>
